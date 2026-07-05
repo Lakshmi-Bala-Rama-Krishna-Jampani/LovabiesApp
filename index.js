a338b995620aa529c1a4fc04be5ba992baaf7909
Lakshmi Bala Rama Krishna Jampani <balaramchowdary0703@gmail.com>
2026-07-05 14:50:28 +0530

    Fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 
 import App from './App';
-import { name as appName } from './app.json';
+
+const appName = require('./app.json').name ?? 'LovabiesApp';
 
 AppRegistry.registerComponent(appName, () => App);
